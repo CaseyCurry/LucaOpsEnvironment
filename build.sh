2c53d4a6731ec43cae556f146249d7b55f08694a
Casey Curry <casey.de.curry@gmail.com>
2017-02-12 15:04:26 -0600

    make build.sh safe for testing

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 vagrant ssh
 
 #install node
-curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-yum install -y nodejs-6.9.5
+#curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+#yum install -y nodejs-6.9.5
+ls /vagrant
 
 logout
