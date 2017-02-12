0f0dc16529e6d8aa4700a2010816d889abdda2cb
Casey Curry <casey.de.curry@gmail.com>
2017-02-12 14:02:55 -0600

    add vagrant

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+vagrant up
+vagrant ssh
+
+#install node
+curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+yum install -y nodejs-6.9.5
+
+logout
+vagrant destroy
