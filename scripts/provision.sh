710c2db8ea1d9091a2cc37e982f6afb0ab07d97e
Casey Curry <casey.de.curry@gmail.com>
2017-02-13 19:11:29 -0600

    update start-application.sh and add pm2 to provision

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,3 +2,4 @@
 curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
 yum install -y nodejs-6.9.5
 yum install -y gcc-c++ make
+npm install -g pm2
--- scripts/start-application.sh
+++ scripts/start-application.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
-node -v
-ls /vagrant
+SERVICE_REGISTRY_API=http://localhost:12001
+
+function start {
+  pm2 start ../$1/host.js --name $2 --watch $1 --output run-logs/$2.output.log --error run-logs/$2.error.log
+}
+
+pm2 delete all
+
+start services/service-registry/api/dist service-registry-api
+start services/service-registry/client/dist service-registry-client
+
+API_STATUS_CODE=$(curl -s -o /dev/null -I -w "%{http_code}" $SERVICE_REGISTRY_API)
+
+if ["$API_STATUS_CODE" != "200"]; then
+  sleep 2s
+fi
+
+start services/categories/api/dist categories-api
+start services/users/api/dist users-api
+start services/users/client/dist users-client
+start services/checking-account/api/dist checking-account-api
+start services/checking-account/client/dist checking-account-client
+
+sleep 1s
+
+start application/dist application
+
+curl http://localhost:12001/api/services
