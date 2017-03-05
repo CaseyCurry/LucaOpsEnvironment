8d7e60150bd8a4d183ceb1b3f4c76ec8c9154849
Casey Curry <casey.de.curry@gmail.com>
2017-03-04 19:40:38 -0600

    add selenium

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# install java
 yum install -y java-1.8.0-openjdk-src.x86_64
 
+# install xvfb
+yum install -y xorg-x11-server-Xvfb.x86_64
+
 # install chrome
-# curl --silent -L https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -o google-chrome-stable_current_x86_64.rpm
-# yum install -y google-chrome-stable_current_x86_64.rpm
+curl --silent -L https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -o google-chrome-stable_current_x86_64.rpm
+yum install -y google-chrome-stable_current_x86_64.rpm
+
 # curl --silent -L http://chrome.richardlloyd.org.uk/install_chrome.sh -o install_chrome.sh
 # chmod u+x install_chrome.sh
 # ./install_chrome.sh -f
-# yum install -y chromedriver.x86_64
+
+yum install -y chromedriver.x86_64
+
 # cp /opt/google/chrome/google-chrome /opt/google/chrome/google-chromedriver
 # sed -i 's#$HERE/chrome#$HERE/chromedriver#g' /opt/google/chrome/google-chromedriver
 
