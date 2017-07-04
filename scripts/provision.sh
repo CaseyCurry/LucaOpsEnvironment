f702f39cba9127e1b17d6ac741dac619cb55245d
Casey Curry <casey.de.curry@gmail.com>
2017-07-04 17:59:42 -0500

    install chrome

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+yum makecache fast
+
 # install chrome
-# curl -L https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -o google-chrome-stable_current_x86_64.rpm
-# yum install -y google-chrome-stable_current_x86_64.rpm
+curl -L https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -o google-chrome-stable_current_x86_64.rpm
+yum install -y google-chrome-stable_current_x86_64.rpm
 # yum install -y chromedriver.x86_64
 
 # curl --silent -L http://chrome.richardlloyd.org.uk/install_chrome.sh -o install_chrome.sh
 # chmod u+x install_chrome.sh
 # ./install_chrome.sh -f
 # yum install https://dl.google.com/linux/direct/google-chrome-stable_current_i386.rpm
-#
-install firefox
-yum install -y firefox.x86_64
-curl -L https://github.com/mozilla/geckodriver/releases/download/v0.14.0/geckodriver-v0.14.0-linux64.tar.gz | tar -xvz geckodriver*
-chmod +x geckodriver
-mv geckodriver /usr/bin
+
+# install firefox
+# yum install -y firefox.x86_64
+# curl -L https://github.com/mozilla/geckodriver/releases/download/v0.14.0/geckodriver-v0.14.0-linux64.tar.gz | tar -xvz geckodriver*
+# chmod +x geckodriver
+# mv geckodriver /usr/bin
 
 # install java
 yum install -y java-1.8.0-openjdk-src.x86_64
