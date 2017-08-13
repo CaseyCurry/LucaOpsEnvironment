f935d582336002efc22cd3dc7b75fe2a36946f1d
Casey Curry <casey.de.curry@gmail.com>
2017-08-12 21:16:33 -0500

    remove argument in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# aws ec2 describe-instances --filter Name=tag:Name,Values=Luca --output text --query 'Reservations[*].Instances[*].State.Name'
-#
-# INSTANCE=$(aws ec2 describe-instances --filter Name=tag:Name,Values=Luca --output text --query 'Reservations[*].Instances[0].InstanceId'); who | aws ec2 start-instance --instance-ids "${INSTANCE#}"
-#
-# ssh -i ~/Documents/LucaInstance.pem centos@34.213.168.62
-#
-# scp -i ~/Documents/LucaInstance.pem ~/Documents/test centos@34.213.168.62:~/
-#
-# IP=$(aws ec2 describe-instances --filter Name=tag:Name,Values=Luca --output text --query 'Reservations[*].Instances[0].PublicIpAddress'); who | scp -o StrictHostKeyChecking=no -i ~/Documents/LucaInstance.pem test2 centos@"${IP#}":~
-#
-# if [ -z `ssh-keygen -F $IP` ]; then
-#   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
-# fi
-
 function get_instance_id {
   # if there are multiple instance where Name=Luca, this only selects the first instance; see the query argument
   instance_id=`aws ec2 describe-instances --filter Name=tag:Name,Values=Luca --output text --query 'Reservations[*].Instances[0].InstanceId'`
@@ -69,28 +55,9 @@ function start_instance {
 }
 
 function transfer_containers {
-  # if [ -z "`ssh-keygen -F $ip`" ]
-  # then
-  #   remove_host_after_transfer=true
-  #   echo adding $ip to known_hosts
-  #   ssh-keyscan -H -t rsa $ip >> /Users/caseycurry/.ssh/known_hosts
-  # fi
-  #
-  # echo transferring containers
-  # rsync -av --delete --progress --ignore-existing -e 'ssh -i /Users/caseycurry/Documents/LucaInstance.pem' '$directory_to_transfer' centos@$ip:~/containers
-  # echo transferred
-  #
-  # if [ $remove_host_after_transfer ]
-  # then
-  #   echo removing $ip from known_hosts
-  #   ssh-keygen -R $ip -f /Users/caseycurry/.ssh/known_hosts
-  # fi
-  # scp -r -o StrictHostKeyChecking=no -i /Users/caseycurry/Documents/LucaInstance.pem '$directory_to_transfer' centos@$ip:~/containers
-  rsync -av --delete --progress --ignore-existing -e 'ssh -o StrictHostKeyChecking=no -i /Users/caseycurry/Documents/LucaInstance.pem' '$directory_to_transfer' centos@$ip:~/containers
+  rsync -av --delete --progress --ignore-existing -e 'ssh -o StrictHostKeyChecking=no -i /Users/caseycurry/Documents/LucaInstance.pem' ./containers/latest centos@$ip:~/containers
 }
 
-directory_to_transfer=`pwd`/$1
-echo $directory_to_transfer
 get_instance_id
 get_instance_state
 
